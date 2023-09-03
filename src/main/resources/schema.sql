a43b76c0ea601d96eb024bcf40a3633c05db1184
Santiago Morales <58920781+Santiagommvv@users.noreply.github.com>
2023-09-02 21:02:02 -0300

    cambios para railway
    actualizado el pom
    actualizada generación llave primaria conforme a postgres
    actualizadas las properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Content (
-    id INTEGER AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description text,
     status VARCHAR(20) NOT NULL,
@@ -7,10 +7,13 @@ CREATE TABLE IF NOT EXISTS Content (
     date_created TIMESTAMP NOT NULL,
     date_updated TIMESTAMP,
     url VARCHAR(255),
-    primary key (id)
 );
 
+--enh2--
+--id INTEGER AUTO_INCREMENT,--
 --INSERT INTO Content(title,description,status,content_type,date_created)--
 --VALUES('Mi posteo de arte','sobre el arte de postear','IDEA','ARTICLE', CURRENT_TIMESTAMP());--
 --INSERT INTO Content(title,description,status,content_type,date_created)--
 --VALUES('Otro posteo de arte','sobre el mismo arte de postear que el anterior','IDEA','ARTICLE', CURRENT_TIMESTAMP());--
+--enh2 termina en--
+--primary key (id)--
